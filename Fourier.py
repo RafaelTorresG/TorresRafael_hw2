7480d87688251403ab33dbdca4545da3b85354cd
Rafael Torres <rf.torres@uniandes.edu.co>
2019-05-05 01:39:28 +0000

    Hechas las gráficas, probadas en notebook de Jupyter, parece hacer lo que se supone que debería hacer

--- Fourier.py
+++ Fourier.py
@@ -5,3 +5,21 @@ import matplotlib.pylab as plt
 #en el segundo la senial esta conformada por, primero una de las ondas y luego la siguiente.
 sigSum=np.genfromtxt('signalSuma.dat')
 sig=np.genfromtxt('signal.dat')
+
+#Haga una grafica con dos subplots uno con los datos de signal.dat y otro con los de signalSuma.dat.
+#Desagregar tiempo y senial
+tSum=sigSum[:,0]
+senialSum=sigSum[:,1]
+
+t=sig[:,0]
+senial=sig[:,1]
+
+plt.figure()
+plt.subplot(1,2,1)
+plt.plot(tSum,senialSum)
+
+plt.figure()
+plt.subplot(1,2,2)
+plt.plot(t,senial)
+
+plt.savefig("PlotFourier1.pdf")
