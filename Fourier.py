a2f30c84ce3be5528c384ae54f892b5afab353de
Rafael Torres <rf.torres@uniandes.edu.co>
2019-05-05 01:29:53 +0000

    Importacion de paquetes y datos punto 1. Probados en notebook de Jupyter, hace lo que deberia hacer

---
+++ Fourier.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+#Almacene los datos de signalSuma.dat y de signal.dat. Estas dos seniales estan conformadas por dos ondas sinusoidales. En el primer caso la #senial es la suma de las dos ondas,
+#en el segundo la senial esta conformada por, primero una de las ondas y luego la siguiente.
+sigSum=np.genfromtxt('signalSuma.dat')
+sig=np.genfromtxt('signal.dat')
