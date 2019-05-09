9e505fb54d27c41d0c549245a2fd4ae1e610279d
Rafael Torres <rf.torres@uniandes.edu.co>
2019-05-09 04:01:55 +0000

    Importa bien los datos del cpp, hecho el slicing

---
+++ Plots_hw2.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pylab as plt
+edificio=np.genfromtxt("edificio.txt",delimiter=",")
+data1=edificio[0:,][0:1000]
+W=[]
+U1max=[]
+U2max=[]
+U3max=[]
+for n in range(100):
+    rango=edificio[0:,][1000*(n+1):1000*(n+2)]
+    u1=rango[:,4]
+    u2=rango[:,5]
+    u3=rango[:,6]
+    w=rango[:,7][0]
+    W.append(w)
+    U1max.append(u1)
+    U2max.append(u2)
+    U3max.append(u3)
