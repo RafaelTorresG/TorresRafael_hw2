59ccd7690ca01e7ea6d1e5c89f484dddb93e2881
Rafael Torres <rf.torres@uniandes.edu.co>
2019-05-09 04:06:18 +0000

    Halla los maximos, falta graficar y analizar no mas :)

--- Plots_hw2.py
+++ Plots_hw2.py
@@ -8,9 +8,9 @@ U2max=[]
 U3max=[]
 for n in range(100):
     rango=edificio[0:,][1000*(n+1):1000*(n+2)]
-    u1=rango[:,4]
-    u2=rango[:,5]
-    u3=rango[:,6]
+    u1=max(abs(rango[:,4]))
+    u2=max(abs(rango[:,5]))
+    u3=max(abs(rango[:,6]))
     w=rango[:,7][0]
     W.append(w)
     U1max.append(u1)
