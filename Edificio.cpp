d8a0af46443f3f9682a61f4a658a383b790c5cd2
Rafael Torres <rf.torres@uniandes.edu.co>
2019-05-09 02:21:07 +0000

    Inicializacion

--- Edificio.cpp
+++ Edificio.cpp
@@ -1,11 +1,46 @@
 #include<iostream>
 #include<math.h>
 
+void * LeapFrog(float float float float float float int float)
+
 float m=1000.0;
 float k=2000.0;
 float w=1.0*sqrt(k/m);
+float a=k/m;
 
 int main()
 {
+ 
  return 0;
+}
+
+void * LeapFrog(float u10 float v10 float u20 float v20 float u30 float v30 int N float h)
+{
+ float *V1;
+ float *V2;
+ float *V3;   
+ float *U1;
+ float *U2;
+ float *U3;  
+ 
+ V1=float [N];
+ V2=float [N];
+ V3=float [N];
+ U1=float [N];
+ U2=float [N];
+ U3=float [N];
+    
+ V1[0]=0;
+ V2[0]=0;
+ V3[0]=0;
+ U1[0]=0;
+ U2[0]=0;
+ U3[0]=0;
+    
+ for(int t=1; t<N; t++)
+ {
+  float V1_old=V1[t-1];
+  float V2_old=V1[t-1];
+  float V3_old=V1[t-1];
+ }
 }
