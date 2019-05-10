a29095bf6e4b205aeb48fef9c2c02bf80095a964
Rafael Torres <rf.torres@uniandes.edu.co>
2019-05-10 10:03:43 -0500

    hecho y funcionando

--- hw2.mk
+++ hw2.mk
@@ -1,29 +1,30 @@
-resultados.pdf : resultados.tex PlotFourier1.pdf PlotTransFourier2.pdf PlotFourier3.pdf PlotFourier4.pdf specgram1.pdf specgram2.pdf specgramTemblor.pdf U_vs_freq.pdf w9.pdf w37.pdf w57.pdf w46.pdf
-    pdflatex resultados.tex
+resultados.pdf : Resultados_hw2.tex PlotFourier1.pdf PlotTransFourier2.pdf PlotFourier3.pdf PlotFourier4.pdf specgram1.pdf specgram2.pdf specgramTemblor.pdf UF.pdf w9.pdf w37.pdf w57.pdf w46.pdf
+	pdflatex Resultados_hw2.tex
 PlotFourier1.pdf : signalSuma.dat signal.dat
-    python Fourier.py
+	python Fourier.py
 PlotTransFourier2.pdf : signalSuma.dat signal.dat
-    python Fourier.py
+	python Fourier.py
 PlotFourier3.pdf : temblor.txt
-    python Fourier.py
+	python Fourier.py
 PlotFourier4.pdf : temblor.txt
-    python Fourier.py  
+	python Fourier.py  
 specgram1.pdf : signalSuma.dat signal.dat
-    python Fourier.py
+	python Fourier.py
 specgram2.pdf : signalSuma.dat signal.dat
-    python Fourier.py
+	python Fourier.py
 specgramTemblor.pdf : temblor.txt
-U_vs_freq.pdf : edificio.txt
-    python Plots_hw2.py
+	python Fourier.py
+UF.pdf : edificio.txt
+	python Plots_hw2.py
 w9.pdf : edificio.txt
-    python Plots_hw2.py
+	python Plots_hw2.py
 w37.pdf : edificio.txt
-    python Plots_hw2.py 
+	python Plots_hw2.py 
 w57.pdf : edificio.txt
-    python Plots_hw2.py 
+	python Plots_hw2.py 
 w46.pdf : edificio.txt
-    python Plots_hw2.py 
+	python Plots_hw2.py 
 edificio.txt : Edificio.cpp a.out
-    ./a.out > edificio.txt
+	./a.out > edificio.txt
 a.out : Edificio.cpp
-    g++ Edificio.cpp
+	g++ Edificio.cpp
