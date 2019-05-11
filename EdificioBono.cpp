8518dc4f16cc383cc6521fd441205afce20e8223
Rafael Torres <rf.torres@uniandes.edu.co>
2019-05-11 03:44:36 +0000

    Toca hacer algo menos ambicioso, se escogen 5 ws y 20 pisos, la idea es ver que pasa en el primero y ultimo piso

--- EdificioBono.cpp
+++ EdificioBono.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<math.h>
 
-void * LeapFrog(float, float, float, float, float, float, int, float, float);
 void * BONO(int, int, float, float);
 
 float m=1000.0;
@@ -12,12 +11,14 @@ float D=w*0.028; //el espacio entre w a probar para graficar amplitudes maximas
 
 int main()
 {
-/* LeapFrog(0,0,0,0,0,0,1000,0.1, w);//Parte 1
- for(int n=0; n<100; n++)
+ for(int n=3; n<23; n++)
  {
-  LeapFrog(0,0,0,0,0,0,1000,0.1,(0.2*w)+(n*D));   //Parte 2
- }*/
- BONO(3,10,0.1,w);
+  BONO(n,1000,0.1,0.5);
+  BONO(n,1000,0.1,1);
+  BONO(n,1000,0.1,1.5);
+  BONO(n,1000,0.1,2);
+  BONO(n,1000,0.1,2.5); 
+ }
  return 0;
 }
 
@@ -83,53 +84,16 @@ void * BONO(int N, int T, float h, float W)
   //Print
  for(int t=0; t<T; t++)
   {
+   std::cout<<t<<","<<N<<","<<W<<","<<U[t][0]<<","<<U[t][N-1]<<std::endl;
+  }
+ /*for(int t=0; t<T; t++)
+  {
    std::cout<<t<<","<<N<<","<<W<<",";
    for(int n=0; n<N-1; n++)
    {
     std::cout<<U[t][n]<<",";   
    }
    std::cout<<U[t][N-1]<<std::endl;
-  }
- }
-//last    
-
-void * LeapFrog(float u10, float v10, float u20, float v20, float u30, float v30, int N, float h, float W)
-{
- float *V1;
- float *V2;
- float *V3;   
- float *U1;
- float *U2;
- float *U3;  
- 
- V1=new float [N];
- V2=new float [N];
- V3=new float [N];
- U1=new float [N];
- U2=new float [N];
- U3=new float [N];
-    
- V1[0]=0;
- V2[0]=0;
- V3[0]=0;
- U1[0]=0;
- U2[0]=0;
- U3[0]=0;
-    
- std::cout<<0<<","<<V1[0]<<","<<V2[0]<<","<<V3[0]<<","<<U1[0]<<","<<U2[0]<<","<<U3[0]<<","<<W<<std::endl;
- for(int t=1; t<N; t++)
- {
-  float V1_old=V1[t-1];
-  float V2_old=V1[t-1];
-  float V3_old=V1[t-1];
-     
-  V1[t]=V1[t-1]+h*(-2*a*U1[t-1]+a*U2[t-1]+sin(W*t*h));
-  V2[t]=V2[t-1]+h*(a*U1[t-1]-2*a*U2[t-1]+a*U3[t-1]);
-  V3[t]=V3[t-1]+h*(a*U2[t-1]-a*U3[t-1]);
-     
-  U1[t]=U1[t-1]+h*V1[t];
-  U2[t]=U2[t-1]+h*V2[t];
-  U3[t]=U3[t-1]+h*V3[t];
-  std::cout<<t<<","<<V1[t]<<","<<V2[t]<<","<<V3[t]<<","<<U1[t]<<","<<U2[t]<<","<<U3[t]<<","<<W<<std::endl;
- }
+  }*/
 }
+//last    
